c55ea3c23e7cd8aa679a00e7daf5c7618da28e6a
anudeep-17 <anudeepsai88@gmail.com>
2023-04-06 02:52:32 -0400

    python classes and objects are used to improve the reusability and make it OOP

--- HW3_SVM_DT/Decisiontree.py
+++ HW3_SVM_DT/Decisiontree.py
@@ -6,99 +6,124 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
-
+import random
 import numpy as np
 
-data = pd.read_csv("cancer-data-train.csv", header = None)
-print(data.iloc[:,:-1])
-X = data.iloc[:,:-1]
-X = X.to_numpy()
-X = StandardScaler().fit_transform(X)
-print(X)
+class decisiontree:
+    f_data_IG = []
+    f_data_gini = []
 
-data[30] = data[30].replace("M", 1)
-data[30] = data[30].replace("B", 0)
-data[30] = data[30].apply(np.int32)
-y = data[30].values
-print(y)
-k = 10
-kf = KFold(n_splits=k)
+    f1_data1_gini = []
+    f1_data2_gini = []
+    f1_data3_gini = []
+    f1_data4_gini = []
+
+    f1_data1_IG = []
+    f1_data2_IG = []
+    f1_data3_IG = []
+    f1_data4_IG = []
+    k = 10
+    kf = KFold(n_splits=k)
+
+    def __init__(self, dataset):
+        print("--------------decision tree working-------------")
+        print()
+        X = self.datasplitter(dataset)[0]
+        y = self.datasplitter(dataset)[1]
+        self.calc(X, y)
+        self.plotter()
+
+    def datasplitter(self, dataset):
+        data = pd.read_csv(dataset, header=None)
+        print(data.iloc[:, :-1])
+        X = data.iloc[:, :-1]
+        X = X.to_numpy()
+        X = StandardScaler().fit_transform(X)
+        print(X)
+
+        data[30] = data[30].replace("M", 1)
+        data[30] = data[30].replace("B", 0)
+        data[30] = data[30].apply(np.int32)
+        y = data[30].values
+        print(y)
+        return (X,y)
+    def calc(self, X, y):
+
+        for train_index, test_index in decisiontree.kf.split(X):
+            X_train, X_test, y_train, y_test = X[train_index], X[test_index], y[train_index], y[test_index]
+
+            DT_gini = DecisionTreeClassifier(criterion="gini", splitter="best", max_leaf_nodes=2, random_state=42)
+            DT_IG = DecisionTreeClassifier(criterion="entropy", splitter="best", max_leaf_nodes=2, random_state=42)
+            DT_gini.fit(X_train, y_train)
+            DT_IG.fit(X_train, y_train)
+            y_predict_gini = DT_gini.predict(X_test)
+            y_predict_IG = DT_IG.predict(X_test)
+            decisiontree.f1_data1_gini.append(f1_score(y_test, y_predict_gini, average='weighted'))
+            decisiontree.f1_data1_IG.append(f1_score(y_test, y_predict_IG, average='weighted'))
+
+            DT_gini = DecisionTreeClassifier(criterion="gini", splitter="best", max_leaf_nodes=5, random_state=42)
+            DT_IG = DecisionTreeClassifier(criterion="entropy", splitter="best", max_leaf_nodes=5, random_state=42)
+            DT_gini.fit(X_train, y_train)
+            DT_IG.fit(X_train, y_train)
+            y_predict_gini = DT_gini.predict(X_test)
+            y_predict_IG = DT_IG.predict(X_test)
+            decisiontree.f1_data2_gini.append(f1_score(y_test, y_predict_gini, average='weighted'))
+            decisiontree.f1_data2_IG.append(f1_score(y_test, y_predict_IG, average='weighted'))
+
+            DT_gini = DecisionTreeClassifier(criterion="gini", splitter="best", max_leaf_nodes=10, random_state=42)
+            DT_IG = DecisionTreeClassifier(criterion="entropy", splitter="best", max_leaf_nodes=10, random_state=42)
+            DT_gini.fit(X_train, y_train)
+            DT_IG.fit(X_train, y_train)
+            y_predict_gini = DT_gini.predict(X_test)
+            y_predict_IG = DT_IG.predict(X_test)
+            decisiontree.f1_data3_gini.append(f1_score(y_test, y_predict_gini, average='weighted'))
+            decisiontree.f1_data3_IG.append(f1_score(y_test, y_predict_IG, average='weighted'))
+
+            DT_gini = DecisionTreeClassifier(criterion="gini", splitter="best", max_leaf_nodes=20, random_state=42)
+            DT_IG = DecisionTreeClassifier(criterion="entropy", splitter="best", max_leaf_nodes=20, random_state=42)
+            DT_gini.fit(X_train, y_train)
+            DT_IG.fit(X_train, y_train)
+            y_predict_gini = DT_gini.predict(X_test)
+            y_predict_IG = DT_IG.predict(X_test)
+            decisiontree.f1_data4_gini.append(f1_score(y_test, y_predict_gini, average='weighted'))
+            decisiontree.f1_data4_IG.append(f1_score(y_test, y_predict_IG, average='weighted'))
+
+        print("---------------------K = 2------------------")
+        print("f1 score for gini",  decisiontree.f1_data1_gini)
+        print("f1 score for IG",  decisiontree.f1_data1_IG)
+        print()
+        print("---------------------K = 5------------------")
+        print("f1 score for gini",  decisiontree.f1_data2_gini)
+        print("f1 score for IG",  decisiontree.f1_data2_IG)
+        print()
+        print("---------------------K = 10------------------")
+        print("f1 score for gini",  decisiontree.f1_data3_gini)
+        print("f1 score for IG",  decisiontree.f1_data3_IG)
+        print()
+        print("---------------------K = 20------------------")
+        print("f1 score for gini",  decisiontree.f1_data4_gini)
+        print("f1 score for IG",  decisiontree.f1_data4_IG)
+        print()
+
+    def Average(self, lst):
+        return sum(lst) / len(lst)
+
+    def plotter(self):
+        # -------------plotting ----------------------------
+        X = ["2", "5", "10", "20"]
+        f_data_IG = [self.Average(decisiontree.f1_data1_IG), self.Average(decisiontree.f1_data2_IG), self.Average(decisiontree.f1_data3_IG), self.Average(decisiontree.f1_data4_IG)]
+        f_data_gini = [self.Average(decisiontree.f1_data1_gini), self.Average(decisiontree.f1_data2_gini), self.Average(decisiontree.f1_data3_gini), self.Average(decisiontree.f1_data4_gini)]
+
+        print("------------averaged f1 score------------------------")
+        print("f1 scores of gini: ", f_data_gini)
+        print("f1 scores of IG: ", f_data_IG)
+        plt.plot(X, f_data_gini, label="GINI")
+        plt.plot(X, f_data_IG, label="IG")
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+        decisiontree.f_data_gini = f_data_gini
+        decisiontree.f_data_IG = f_data_IG
 
 
-f1_data_IG = []
-f1_data_gini = []
-
-f1_data1_gini = []
-f1_data2_gini = []
-f1_data3_gini = []
-f1_data4_gini = []
-
-
-f1_data1_IG = []
-f1_data2_IG = []
-f1_data3_IG = []
-f1_data4_IG = []
-
-
-
-for train_index, test_index in kf.split(X):
-    X_train, X_test, y_train, y_test = X[train_index], X[test_index],y[train_index], y[test_index]
-
-    print("---------------------K = 2------------------")
-    DT_gini = DecisionTreeClassifier(criterion="gini", splitter="best", max_leaf_nodes=2)
-    DT_IG = DecisionTreeClassifier(criterion="entropy", splitter="best", max_leaf_nodes=2)
-    DT_gini.fit(X_train, y_train)
-    DT_IG.fit(X_train, y_train)
-    y_predict_gini = DT_gini.predict(X_test)
-    y_predict_IG = DT_IG.predict(X_test)
-    f1_data1_gini.append(f1_score(y_test, y_predict_gini,average='weighted'))
-    f1_data1_IG.append(f1_score(y_test, y_predict_IG,average='weighted'))
-    print()
-    print("---------------------K = 5------------------")
-    DT_gini = DecisionTreeClassifier(criterion="gini", splitter="best", max_leaf_nodes=5)
-    DT_IG = DecisionTreeClassifier(criterion="entropy", splitter="best", max_leaf_nodes=5)
-    DT_gini.fit(X_train, y_train)
-    DT_IG.fit(X_train, y_train)
-    y_predict_gini = DT_gini.predict(X_test)
-    y_predict_IG = DT_IG.predict(X_test)
-    f1_data2_gini.append(f1_score(y_test, y_predict_gini, average='weighted'))
-    f1_data2_IG.append(f1_score(y_test, y_predict_IG, average='weighted'))
-    print()
-    print("---------------------K = 10------------------")
-    DT_gini = DecisionTreeClassifier(criterion="gini", splitter="best", max_leaf_nodes=10)
-    DT_IG = DecisionTreeClassifier(criterion="entropy", splitter="best", max_leaf_nodes=10)
-    DT_gini.fit(X_train, y_train)
-    DT_IG.fit(X_train, y_train)
-    y_predict_gini = DT_gini.predict(X_test)
-    y_predict_IG = DT_IG.predict(X_test)
-    f1_data3_gini.append(f1_score(y_test, y_predict_gini, average='weighted'))
-    f1_data3_IG.append(f1_score(y_test, y_predict_IG, average='weighted'))
-    print()
-    print("---------------------K = 20------------------")
-    DT_gini = DecisionTreeClassifier(criterion="gini", splitter="best", max_leaf_nodes=20)
-    DT_IG = DecisionTreeClassifier(criterion="entropy", splitter="best", max_leaf_nodes=20)
-    DT_gini.fit(X_train, y_train)
-    DT_IG.fit(X_train, y_train)
-    y_predict_gini = DT_gini.predict(X_test)
-    y_predict_IG = DT_IG.predict(X_test)
-    f1_data4_gini.append(f1_score(y_test, y_predict_gini, average='weighted'))
-    f1_data4_IG.append(f1_score(y_test, y_predict_IG, average='weighted'))
-    print()
-
-def Average(lst):
-    return sum(lst) / len(lst)
-
-
-#-------------plotting ----------------------------
-X =["2","5","10","20"]
-f1_data_IG = [Average(f1_data1_IG),Average(f1_data2_IG),Average(f1_data3_IG), Average(f1_data4_IG)]
-f1_data_gini = [Average(f1_data1_gini),Average(f1_data2_gini),Average(f1_data3_gini), Average(f1_data4_gini)]
-
-print("------------averaged f1 score------------------------")
-print(f1_data_gini)
-print(f1_data_IG)
-plt.plot(X,f1_data_gini, label = "GINI")
-plt.plot(X,f1_data_IG, label = "IG")
-plt.legend()
-plt.tight_layout()
-plt.show()
+decisiontreetest = decisiontree("cancer-data-train.csv")
