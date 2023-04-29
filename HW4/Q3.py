f6f9b9ba2705c5f6d61c6fd2bbc948bb5c477e82
anudeep-17 <anudeepsai88@gmail.com>
2023-04-28 20:19:00 -0400

    checkout

--- HW4/Q3.py
+++ HW4/Q3.py
@@ -47,16 +47,56 @@ print("eigenvector values for Ls: ", eigenvectors1[min_index])
 U_s = eigenvectors1[min_index]
 
 node_id = [1,2,3,4,5,6]
-
-plt.plot(node_id, U, label = "values for L")
-plt.plot(node_id, U_s, label = "values for Ls")
-
-plt.xlabel('node_id')
-plt.ylabel('eigenvector values')
-plt.title('3B question')
-
-# add a legend
-plt.legend()
+#
+# plt.plot(node_id, U, label = "values for L")
+# plt.plot(node_id, U_s, label = "values for Ls")
+#
+# plt.xlabel('node_id')
+# plt.ylabel('eigenvector values')
+# plt.title('3B question')
+#
+# # add a legend
+# plt.legend()
 
 # display the graph
-plt.show()
+# plt.show()
+
+import math
+
+def eigencalc( i, j):
+    dict = {}
+    for check in range(0, len(U)):
+        if (j == len(U)):
+            break;
+        dict[i + 1, j + 1] = math.sqrt(math.pow(((j + 1) - (i + 1)), 2) + math.pow((U[i] - U[j]), 2))
+        j = j + 1
+    return dict
+
+def euclidcalc(val, val2, x, y):
+    return math.sqrt(math.pow((y-x),2) + math.pow((val2-val),2))
+
+
+print("for point 1", eigencalc(0,1))
+print("for point 2", eigencalc(1,2))
+print("for point 3", eigencalc(2,3))
+print("for point 4", eigencalc(3,4))
+print("for point 5", eigencalc(4,5))
+
+
+def eigencalc_s( i, j):
+    dict = {}
+    for check in range(0, len(U_s)):
+        if (j == len(U_s)):
+            break;
+        dict[i + 1, j + 1] = math.sqrt(math.pow(((j + 1) - (i + 1)), 2) + math.pow((U_s[i] - U_s[j]), 2))
+        j = j + 1
+    return dict
+
+print()
+print(" -----------------------------------------------")
+
+print("for point 1", eigencalc_s(0,1))
+print("for point 2", eigencalc_s(1,2))
+print("for point 3", eigencalc_s(2,3))
+print("for point 4", eigencalc_s(3,4))
+print("for point 5", eigencalc_s(4,5))
